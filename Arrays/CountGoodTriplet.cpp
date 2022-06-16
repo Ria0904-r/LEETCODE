4d374dd67dbe19bbc389b5a0bcd0efbd29e585a6
Ria Sehgal <riasehgal48@gmail.com>
2022-06-16 13:59:58 +0530

    Update CountGoodTriplet.cpp

--- Arrays/CountGoodTriplet.cpp
+++ Arrays/CountGoodTriplet.cpp
@@ -18,18 +18,16 @@
   class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int count=0;
-        for(int i=0;i<arr.size()-2;i++){
-            for(int j=i+1;j<arr.size();j++){
-                if(arr[i]-arr[j]<=a){
-                    for(int k=j+1;k<arr.size();k++){
-                        if(abs(arr[j]-arr[k])<=b && (abs(arr[i]-arr[k])<=c)){
-                            count++;
-                        }
-                    }
-                }
-            }
-        }
+        int i,j,k,count=0;
+        for(i=0;i<arr.size()-2;i++){
+            for(j=i+1;j<arr.size()-1;j++){
+                if(abs(arr[i]-arr[j])<=a){  
+                for(k=j+1;k<arr.size();k++){
+                    if( abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c){
+                        count++;
+            }}}}}
+            
+        
         return count;
     }
 };
