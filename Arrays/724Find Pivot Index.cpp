208a788f0bb1ee1d0669ac1409c4c9d11e39bd78
Ria Sehgal <riasehgal48@gmail.com>
2022-06-18 07:12:40 +0530

    Create 724Find Pivot Index.cpp

---
+++ Arrays/724Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rsum= accumulate(nums.begin(),nums.end(),0);
+        int lsum=0;
+        
+        for(int i=0;i<nums.size();i++){
+            rsum=rsum-nums[i];
+            if(lsum==rsum){
+                return i;
+            }
+            lsum=lsum+nums[i];
+        }
+            return -1;
+    }
+};
